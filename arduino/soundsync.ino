4de6e1ae8ebe4ce142e1de4604bc12f8341affc1
kan-fumihito <42938686+kan-fumihito@users.noreply.github.com>
2020-07-09 14:51:39 +0900

    Add files via upload

---
+++ arduino/soundsync.ino
@@ -0,0 +1,77 @@
+char sw=0,bsw='j';
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(i=2;i<15;i++)pinMode(i,INPUT);//2-14
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sw=0;
+
+  if(digitalRead(2)){
+    sw='a';
+    goto A;
+    
+  }
+  if(digitalRead(3)){
+    sw='b';
+    goto A;
+  }
+  if(digitalRead(4)){
+    sw='c';
+    goto A;
+  }
+  if(digitalRead(5)){
+    sw='d';
+    goto A;
+  }
+  if(digitalRead(6)){
+    sw='e';
+    goto A;
+  }
+  
+  if(digitalRead(7)){
+    sw='f';
+    goto A;
+  }
+  
+  if(digitalRead(8)){
+    sw='g';
+    goto A;
+  }
+  if(digitalRead(9)){
+    sw='h';
+    goto A;
+  }
+  if(digitalRead(10)){
+    sw='i';
+    goto A;
+  }
+  if(digitalRead(11)){
+    sw='k';
+    goto A;
+  }
+  if(digitalRead(12)){
+    sw='l';
+    goto A;
+  }
+  if(digitalRead(13)){
+    sw='m';
+    goto A;
+  }
+  if(digitalRead(14)){
+    sw='j';
+    goto A;
+  }
+
+  A:
+  
+  if(sw!=bsw){
+    if(sw!=0) Serial.write(sw);
+    bsw=sw;
+  }
+  delay(50);
+}
